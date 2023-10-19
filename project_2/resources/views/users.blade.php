96ad22b7ab38a91ff3f4acfd56e028b21af75c1c
MidnightKyuba <jakubkittel59@gmail.com>
2023-10-19 16:35:09 +0200

    routing lesson

---
+++ project_2/resources/views/users.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/usersStyle.css')}}">
+    <title>Users</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Imię</th>
+            <th>Nazwisko</th>
+            <th>Miasto</th>
+        </tr>
+        @foreach($users as $user)
+            <tr>
+                <td>{{$user['firstName']}}</td>
+                <td>{{$user['lastName']}}</td>
+                <td>{{$user['city']}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
--- project_2/routes/web.php
+++ project_2/routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,4 +14,77 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/show_name', function(){
+    return 'text';
+});
+
+Route::get('/show_blade_city/{city}', function(string $city){
+    return View('city', ['cityName'=>$city]);
+});
+
+Route::get('/pages/{page}', function(string $page){
+    $pages = [
+        'home' => 'strona domowa',
+        'contact' => 'zaq@wp.pl',
+        'adres' => 'Poznań, ul. Grunwaldzka 12000',
+    ];
+    return $pages[$page];
+});
+
+Route::get('/views/{view}', function(string $view){
+    $views = [
+        'strona_glowna' => 'welcome',
+        'name' => 'profile.name',
+    ];
+    return View($views[$view] ?? 'welcome');
+});
+
+Route::get('/views/{view}/{city}', function(string $view, string $city){
+    $views = [
+        'city' => 'city',
+    ];
+    return View($views[$view] ?? 'welcome', ['cityName'=>$city]);
+});
+
+Route::get('/address/{city?}/{street?}/{zipCode?}', function(string $city = null, string $street = null, int $zipCode = null){
+    if($zipCode !== null){
+        $zipCode = substr($zipCode, offset:0, length:2).'-'.substr($zipCode, offset:2, length:3).';';
+    }
+    echo <<< ADDRESS
+        Miasto: $zipCode $city<br>
+        Ulica: $street<hr>
+    ADDRESS; 
+})->name('address');
+
+Route::redirect('/adres/{city?}/{street?}/{zipCode?}','/address/{city?}/{street?}/{zipCode?}');
+
+Route::prefix('admin')->group(function(){
+    
+    Route::get('/home', function(){
+        return 'Strona domowa administratora';
+    });
+
+    Route::get('/users', function(){
+        return 'Użytkownicy';
+    });
+});
+
+Route::get('/films', function(){
+    $films = [
+        'nameFilm1' => 'film1',
+        'nameFilm2' => 'film2',
+        'nameFilm3' => 'film3',
+    ];
+    return View('films', ['films'=>$films]);
+});
+
+Route::get('/users', function(){
+    $users = [
+        ['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city'=>'Poznań'],
+        ['firstName' => 'Anna', 'lastName' => 'Nowak', 'city'=>'Poznań'],
+        ['firstName' => 'Paweł', 'lastName' => 'Nowak', 'city'=>'Gniezno'],
+    ];
+    return View('users', ['users'=>$users]);
 });
